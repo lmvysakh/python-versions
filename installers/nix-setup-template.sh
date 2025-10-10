1ca1cdec3ff0910fb564a86499c670f02835c967
lmvysakh <lmvysakh@github.com>
2025-10-10 14:34:41 +0530

    Alternate solution #4

--- installers/nix-setup-template.sh
+++ installers/nix-setup-template.sh
@@ -51,7 +51,14 @@ chmod +x ../python $PYTHON_MAJOR $PYTHON_MAJOR_DOT_MINOR $PYTHON_MAJORMINOR pyth
 
 echo "Upgrading pip..."
 export PIP_ROOT_USER_ACTION=ignore
-./python -m ensurepip
+
+if ./python -c "import pip; print(pip.__version__)" >/dev/null 2>&1; then
+  echo "pip is available in this interpreter, skipping ensurepip."
+else
+  echo "pip not available, running ensurepip..."
+  ./python -m ensurepip
+fi
+
 ./python -m pip install --upgrade --force-reinstall pip --disable-pip-version-check --no-warn-script-location
 
 echo "Create complete file"
